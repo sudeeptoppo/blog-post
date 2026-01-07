ac258bc1dda1112d56293b4b12c5b8ea74640446
Sudeep Toppo <sudeeptoppo27@email.com>
2026-01-07 12:55:12 +0530

    blog type error full blog cant take undefined

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,16 +1,16 @@
 import { FullBlog } from "../components/FullBlog";
 import { useBlog } from "../hooks";
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { LoadingSkeleton2 } from "../components/LoadingSkeleton2";
 
 export function Blog() {
-    // How to create dynamic routes with react-router-dom?
-    const {id} = useParams(); 
-    const {loading, blog} = useBlog({id: id as string});
+  // How to create dynamic routes with react-router-dom?
+  const { id } = useParams();
+  const { loading, blog } = useBlog({ id: id as string });
 
-    if(loading) {
-        return <LoadingSkeleton2 />;
-    }
+  if (loading || !blog) {
+    return <LoadingSkeleton2 />;
+  }
   return (
     <div>
       <FullBlog blog={blog} />
